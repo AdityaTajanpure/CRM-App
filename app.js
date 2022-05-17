49fb288d1611eb0e92b4e79974dc934a4d92316b
AdityaTajanpure <adityat@accucia.co>
2022-05-17 11:50:51 +0530

    minor change

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ MongoConnection.connect();
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
 var allowedDomains = [
-  "https://crm-app-aditya.netlify.app/",
+  "https://crm-app-aditya.netlify.app",
   "http://localhost:3000",
 ];
 app.use(
